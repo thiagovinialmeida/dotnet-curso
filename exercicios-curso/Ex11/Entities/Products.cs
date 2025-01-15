0aad19ebc63de6c04adf3f40f698f40772a5b77e
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-01-15 17:42:10 -0300

    Estudo sobre System IO

---
+++ exercicios-curso/Ex11/Entities/Products.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Ex11.Entities
+{
+    internal class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Products() { }
+
+        public Products(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("==========================");
+            sb.AppendLine($"- Name: {Name.ToUpper()}");
+            sb.AppendLine($"- Price: {Total():F2}");
+            sb.AppendLine("==========================");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ exercicios-curso/Ex11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using Ex11.Entities;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Products> prod = new List<Products>();
+                string path = "C:\\Users\\SAMSUNG\\Documents\\dev\\dotnet-curso\\exercicios-curso\\Ex11\\products.csv";
+                string[] lines = File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    string[] cut = line.Split(',');
+                    string name = cut[0];
+                    double price = double.Parse(cut[1], CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(cut[2]);
+
+                    prod.Add(new Products(name, price, quantity));
+                }
+
+                Directory.CreateDirectory("C:\\Users\\SAMSUNG\\Documents\\dev\\dotnet-curso\\exercicios-curso\\Ex11\\Out");
+                string targetPath = "C:\\Users\\SAMSUNG\\Documents\\dev\\dotnet-curso\\exercicios-curso\\Ex11\\Out\\summary.csv";
+
+                if (File.Exists(targetPath))
+                {
+                    File.Delete(targetPath);
+                    foreach (Products product in prod)
+                    {
+                        Console.WriteLine(product);
+                        using (StreamWriter sw = File.AppendText(targetPath))
+                        {
+                            sw.WriteLine($"{product.Name},{product.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (Products product in prod)
+                    {
+                        Console.WriteLine(product);
+                        using (StreamWriter sw = File.AppendText(targetPath))
+                        {
+                            sw.WriteLine($"{product.Name},{product.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
